38e6d8bb12c31ffe1c0f6e5ca6c1f8342ba66770
Marcel Cruz <marcelcruz@live.com>
2021-08-02 16:35:26 +0200

    Remove link to API endpoint and add user ID fetch

--- src/components/greet-user.js
+++ src/components/greet-user.js
@@ -1,10 +1,32 @@
 import * as React from "react"
 import { useUser } from "@clerk/clerk-react"
+import axios from "axios"
 
 const GreetUser = () => {
   const user = useUser()
+  const [userId, setUserId] = React.useState(null)
 
-  return <h2>Hi, {user.firstName}</h2>
+  React.useEffect(() => {
+    axios
+      .get("/api/get-user-id")
+      .then(({ data }) => {
+        setUserId(data.id)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <>
+      <h2>Hi, {user.firstName}</h2>
+      {userId && (
+        <p>
+          Your user ID is <strong>{userId}</strong>
+        </p>
+      )}
+    </>
+  )
 }
 
 export default GreetUser
